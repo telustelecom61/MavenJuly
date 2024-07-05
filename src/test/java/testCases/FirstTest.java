f30e25e280712ef3328d9107c8b505534a6773a2
om.prakash.chowdam <om.prakash.chowdam@accenture.com>
2024-07-05 15:00:03 +0530

    First commit

---
+++ src/test/java/testCases/FirstTest.java
@@ -0,0 +1,33 @@
+package testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+
+	@Test
+	public void testGoogle() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://www.saucedemo.com/");
+		Thread.sleep(1000);
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		System.out.println("Clicked on Login");
+		WebElement menu = driver.findElement(By.xpath("//button[@id='react-burger-menu-btn']"));
+		String menuText = menu.getText();
+		Thread.sleep(1000);
+		System.out.println(menuText);
+		driver.quit();
+	}
+}
