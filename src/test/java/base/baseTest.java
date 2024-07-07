e5486c5caa5dfbeb10f3a53451ccb6c7e37d4ace
om.prakash.chowdam <om.prakash.chowdam@accenture.com>
2024-07-07 18:53:07 +0530

    added basetest with params for jenkins

---
+++ src/test/java/base/baseTest.java
@@ -0,0 +1,39 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class baseTest {
+
+	public WebDriver driver;
+	@BeforeTest
+	public void setup() {
+		if(System.getProperty("browser").equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
+			driver = new ChromeDriver(options);
+		}
+		else if(System.getProperty("browser").equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			EdgeOptions options = new EdgeOptions();
+			options.addArguments("--headless");
+			driver = new EdgeDriver(options);
+		}
+		driver.manage().window().maximize();
+		driver.get(System.getProperty("url"));
+	}
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	
+	
+}
